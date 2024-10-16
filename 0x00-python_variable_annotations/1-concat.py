4455dec90dc5d9b40442484929e328c06ff7daf1
Favour Idowu <favouridowu02@gmail.com>
2024-10-16 09:55:18 +0100

    Add a file that contains a function concat
    Implemented a function concat that accepts two arguments str(String) and str2(String). returns a concatinated string of str1 and str2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+    This Module contains a function concat That concatinate the value
+    of two strings that a passed
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        This Function is used to concatinate two string.
+
+        Arguments:
+            str1: The first string
+            str2: The Second string
+    """
+    return "{}{}".format(str1, str2)
