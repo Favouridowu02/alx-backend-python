81ac49fa6b17d44d98bbda4152ef6edd608abd11
Favour Idowu <favouridowu02@gmail.com>
2024-10-16 10:13:17 +0100

    Add a file that contains a function to_str
    Implemented a function that accepts one argument n(Float) and returns the string representation of it

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    This Module contains a function to_str. That returns the string
+    representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        This Function returns a string representation of a float
+
+        Arguments:
+            n: The float value
+        Return: This returns the string representation of the float
+    """
+    return str(float)
