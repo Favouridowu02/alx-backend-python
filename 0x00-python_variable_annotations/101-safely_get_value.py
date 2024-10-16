3abcad9ff6164df3c071e4dbf1422d5bb01fc8a0
Favour Idowu <favouridowu02@gmail.com>
2024-10-16 23:23:21 +0100

    Updated this file to make it pycodestyle and mypy complaint

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,12 +2,14 @@
 """
     This Module contains a type-annotated function like safely_get_value
 """
-from typing import Union, Mapping, Any, TypeVar
+from typing import Union, Mapping, Any, TypeVar, TypeAlias
 
-T = TypeVar('~T')
+T = TypeVar('T')
+D: TypeAlias = Union[T, None]
+R: TypeAlias = Union[Any, T]
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: D = None) -> R:
     """
         This is an annotated function that return
 
