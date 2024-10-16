b7f131ee5868d253d927e9f8dce8cc9e8371e175
Favour Idowu <favouridowu02@gmail.com>
2024-10-16 10:03:59 +0100

    Add a file that contains a function floor
    Implemented a function floor that accepts a floor and returns an integer value of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    This Module contains a function floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+        This Function returns the floor value of a float
+
+        Arguments:
+            n: This is the float value
+
+        Return: This returns the floor value of the float
+    """
+    return math.floor(n)
