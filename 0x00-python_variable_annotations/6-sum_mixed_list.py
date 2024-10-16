1f226b2d340ba9ba43f7465c757db75bdbda0960
Favour Idowu <favouridowu02@gmail.com>
2024-10-16 13:29:26 +0100

    Add a file that contains a function
    Implemented a function that accepts an argument mxd_lst[List(int | float)]. It returns the sum of the list in float

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,10 +3,10 @@
     This Model contains a function sum_mixed_list that accepts a
     list of float and int
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
         This function that accepts a list of int and float
 
