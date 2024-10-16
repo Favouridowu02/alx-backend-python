c8e142f49c3d39220308674138c84a48ab04dac9
Favour Idowu <favouridowu02@gmail.com>
2024-10-16 09:31:11 +0100

    Add a file that contains a function add
    implemented a function add that returns the value of two added variables

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    This Module contains a function add That accepts two float
+    parameters
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        This Function is for addition
+
+        Arguments:
+            a: This is the first variable of type float
+            b: This is the second variable of type float
+
+        Return: This returns the addition of both variables in float
+    """
+    return (a + b)
