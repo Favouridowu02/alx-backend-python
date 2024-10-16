faae8618fa04f685703cff033cfaa22ed6ee4e03
Favour Idowu <favouridowu02@gmail.com>
2024-10-16 23:56:07 +0100

    Add a file 102-type_checking.py that contains a function zoom_array
    Implemented a function zoom_array. It accepts two arguments lst[tuple] and factor[int] with default value of 2. It returns a List

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    This Module contains an annotated function zoom_array
+"""
+from typing import List, Tuple, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+#        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array: Tuple[Any, ...] = (12, 72, 91)
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, int(3.0))
