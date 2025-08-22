f75509a21a43e65f32d95b68e6084d6e992a0717
Favour Idowu <favouridowu02@gmail.com>
2025-08-22 14:14:08 +0100

    -

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
 """
+Batch processing users from a database using SQLAlchemy
 """
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
 
-import csv
+load_dotenv()
+DB_URI = os.environ.get("DB_URI")
 
 def stream_users_in_batches(batch_size):
     """
-    Generator that yields batches of user rows from user_data.csv as dicts.
+    Generator that yields batches of user rows from the users table as dicts.
     Each batch is a list of dicts with length up to batch_size.
     """
-    batch = []
-    with open("user_data.csv", "r", newline='') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            batch.append(row)
-            if len(batch) == batch_size:
-                yield batch
-                batch = []
-        if batch:
+    engine = create_engine(DB_URI)
+    with engine.connect() as conn:
+        offset = 0
+        while True:
+            result = conn.execute(
+                text("SELECT * FROM users LIMIT :limit OFFSET :offset"),
+                {"limit": batch_size, "offset": offset}
+            )
+            batch = [dict(row) for row in result]
+            if not batch:
+                break
             yield batch
+            offset += batch_size
 
 def batch_processing(batch_size):
     """
-        This function processes each batch to filter users over the age of 25
+    This function processes each batch to filter users over the age of 25
 
-        Arguments:
-            batch_size: the batch size
+    Arguments:
+        batch_size: the batch size
     """
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
@@ -35,5 +43,3 @@ def batch_processing(batch_size):
                 age = 0
             if age > 25:
                 print(user)
-
- 
