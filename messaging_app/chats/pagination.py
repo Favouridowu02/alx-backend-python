6d174e988a169053cba1f7cca03da01643705c09
Favour Idowu <favouridowu02@gmail.com>
2025-10-08 11:17:09 +0100

    .

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,9 +1,12 @@
-import PageNumberPagination from rest_framework.pagination
+from rest_framework.pagination import PageNumberPagination
 
 class MessagePagination(PageNumberPagination):
     """
-        Message Pagination Class
+    Message Pagination Class - returns 20 messages per page by default.
     """
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+
+# "page.paginator.count"
